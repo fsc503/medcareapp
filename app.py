39cb3f95472c8f600c7d1c5d3dc74632aeffe5ee
fsc503 <82701461+fsc503@users.noreply.github.com>
2021-04-21 19:32:37 +0530

    Update app.py
    To define database name and db value

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 # app.py
 
 from flask import Flask
-#from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///healthcare.db'
 app.secret_key = "flask rocks!"
 
